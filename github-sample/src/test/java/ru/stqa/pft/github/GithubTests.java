1ea230179dc71b68bc78fc75227ad5a421064602
ivanrightman <vivasvse@gmail.com>
2020-05-08 18:51:16 +0500

    lection 9.6

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+
+public class GithubTests {
+
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub(" ");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("ivanrightman", "java_tests")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            //System.out.println(commit);
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+}
